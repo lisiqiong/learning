ab72003b0d7040f04392f6b2f786256bdb9cf66a
lisiqiong <peterlee20100427@yeah.net>
2016-06-01 21:15:32 -0700

    修改redis类

--- s_redis/index.php
+++ s_redis/index.php
@@ -2,6 +2,9 @@
 header('Content-Type:text/html;charset=utf-8;');
 require_once('redis.class.php');
 $redis = new MyRedis();
-$res = $redis->get('test1');
-echo "键值为test1的值为：".$res;
+//$res = $redis->get('test1');
+//echo "键值为test1的值为：".$res;
+//$redis->set('test2','test two  hhh');
+//$redis->set('test3');
+print_r($redis->get('test2'));
 ?>
--- s_redis/redis.class.php
+++ s_redis/redis.class.php
@@ -5,6 +5,7 @@
  **/
 class MyRedis{
     private $redis;
+    public $error;
     /*
      *@desc 实例化redis类
      **/
@@ -14,9 +15,30 @@ class MyRedis{
         return $this->redis;
     }
   
+    /*
+     *@desc 设置字符串类型的值，以及失效时间
+     **/
+    public function set($key,$value=0,$timeout=0){
+        if(empty($value)){
+            $this->error = "设置键值不能够为空哦~";
+            return $this->error;
+        }
+        $res = $this->redis->set($key,$value);
+        if($timeout){
+            $this->redis->expire($key,$timeout);
+        }
+        return $res;
+    }
+
+    /**
+     *@desc 获取字符串类型的值
+     **/
     public function get($key){
         return $this->redis->get($key);
     }
+
+    
+
 
 }
 
